da9957eec4765fbb112b8c55f8bd749f8c3ca28a
vaxevanis <vaxevanis.paul@gmail.com>
2019-04-01 16:51:07 +0300

    Feat(Counter) FormatCount
    // introduce our first function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,20 +9,24 @@ class Counter extends Component {
       <div className="container">
         <div className="col-lg-6 Counter">
           <div className="input-group">
-            <span>{this.state.count}</span>
+            <span>{this.formatCount()}</span>
             <button className="btn btn-primary">Increment</button>
           </div>
           <div className="input-group">
-            <span>{this.state.count}</span>
+            <span>{this.formatCount()}</span>
             <button className="btn btn-primary">Increment</button>
           </div>
           <div className="input-group">
-            <span>{this.state.count}</span>
+            <span>{this.formatCount()}</span>
             <button className="btn btn-primary">Increment</button>
           </div>
         </div>
       </div>
     );
+  } //render
+
+  formatCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
